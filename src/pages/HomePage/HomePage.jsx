28d3d1a8ce7d51232ba77bcb8aa3447fb4bae975
Volodymyr Popovichenko <vovvva33@gmail.com>
2020-05-25 09:36:08 +0300

    project section start

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+import Routes from "../../configs/routes";
 import { Helmet } from "react-helmet";
+import { useHistory } from "react-router";
 import Button from "../../components/common/Button";
 import bg from "../../assets/img/bg_home_design.png";
 
 import seo from "../../configs/seo";
 import s from "./style.module.scss";
+
+const ServiceItem = ({ path, title = "", valueButton, onClick = () => {} }) => {
+  const history = useHistory();
+  const click = () => {
+    onClick();
+    setTimeout(() => history.push(path), 500);
+  };
+  return (
+    <div className={s.service}>
+      <h2 className={s.subTitle}>{title}</h2>
+      <Button onClick={click} theme="yellow">
+        {valueButton || "Подробнее..."}
+      </Button>
+    </div>
+  );
+};
 
 const HomePage = () => {
   return (
@@ -13,22 +31,29 @@ const HomePage = () => {
         <title>{seo.HOME.title}</title>
         <meta name="description" content="Helmet application" />
       </Helmet>
-      <div className={s.imgBG}>
-        <img src={bg} alt="design" />
-      </div>
-      <h1 className={s.title}>
-        <span> graphic </span>
-        <span> branding </span>
-        <span> web </span>
-      </h1>
-      <p className={s.title__after}>
-        ... для ценителей <br /> дизайна со смыслом
-      </p>
-      <div className={s.service}>
-        {/* <h2 className="s"></h2> */}
-        <Button></Button>
-      </div>
-      {/* <h2 className="s"></h2> */}
+      <section className={s.headSection}>
+        <div className={s.imgBG}>
+          <img src={bg} alt="design" />
+        </div>
+        <h1 className={s.title}>
+          <span> graphic </span>
+          <span> branding </span>
+          <span> web </span>
+        </h1>
+        <p className={s.title__after}>
+          ... для ценителей <br /> дизайна со смыслом
+        </p>
+      </section>
+      <section className={s.servicesSection}>
+        <ServiceItem path={Routes.GRAPHIC} title="Graphic design" />
+        <ServiceItem path={Routes.WEB} title="Web design" />
+      </section>
+      <section className={s.projectSection}> 
+        <h2 className={s.titleSection}>
+          Project
+        </h2>
+        
+      </section>
     </div>
   );
 };
